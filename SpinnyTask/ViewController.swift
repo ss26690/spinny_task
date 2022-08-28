b4502255b121aeea6bbe01af7ac8059556b947cd
Saurav Sagar <sagarsaurav1997@gmail.com>
2022-08-28 20:16:22 +0530

    initial setup

---
+++ SpinnyTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpinnyTask
+//
+//  Created by Saurav Sagar on 28/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
